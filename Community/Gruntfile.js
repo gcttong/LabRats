3e75c0b77c8be0a19a28700aa4b04a1ec6445a29
brendan <bvincelli18@gmail.com>
2017-05-17 15:32:12 -0400

    Added Main Page

---
+++ Community/Gruntfile.js
@@ -0,0 +1,11 @@
+/* 
+ Student: Brendan Vincelli <vincellb@sheridan.desire2learn.com>
+ StudentID: 991433197
+ Course: INFO16206
+ Project: Community
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ Community/gulpfile.js
@@ -0,0 +1,12 @@
+/* 
+ Student: Brendan Vincelli <vincellb@sheridan.desire2learn.com>
+ StudentID: 991433197
+ Course: INFO16206
+ Project: Community
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
